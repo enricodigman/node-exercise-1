cdebf43c0a1d009a5cdf4ade2afb891c0082dfad
Enrico Digman <essdigman@gmail.com>
2022-10-14 07:13:06 +0800

    Implemented early returns and readable variable names

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
 const fs = require('fs')
-const process = require('process')
 
-const argument = process.argv.slice(2)
+const [,, contentFile, outputFile, option] = process.argv
 
-if (argument[0] === undefined || argument[1] === undefined) {
+const result = []
+if (contentFile === undefined || outputFile === undefined) {
   console.log('Missing arguments')
-} else {
-  fs.access(argument[1].toString(), fs.F_OK, (state) => {
-    const result = []
-    if (!state) {
-      if (argument[2] === undefined || argument[2] === '-n') {
-        console.log('The file already exists.')
-      } else if (argument[2] === '-y') {
-        fs.readFile(argument[0].toString(), 'utf8', (error, data) => {
-          const line = data.split(/\n/)
-          for (let i = 0; i < line.length; i += 1) {
-            result[i] = `${i + 1}: ${line[i]}`
-          }
-          fs.writeFileSync(argument[1].toString(), result.join('\n'), {
-            encoding: 'utf8',
-            flag: 'w',
-          })
-          console.log('Overwriting of file is done.')
-        })
-      } else {
-        console.error('Third argument is only limited to -y and -n.')
-      }
-    } else {
-      fs.readFile(argument[0].toString(), 'utf8', (error, data) => {
-        const line = data.split(/\n/)
-        for (let i = 0; i < line.length; i += 1) {
-          result[i] = `${i + 1}: ${line[i]}`
-        }
-        fs.writeFileSync(argument[1].toString(), result.join('\n'), {
-          encoding: 'utf8',
-          flag: 'w',
-        })
-        console.log('Created ouput file')
-      })
+  process.exit(1)
+}
+if (fs.existsSync(outputFile)) {
+  if (option !== '-y' && option !== '-n' && option !== undefined) {
+    console.log('Third argument is only limited to -y and -n.')
+    process.exit(1)
+  }
+  if (option === undefined || option === '-n') {
+    console.log('The file already exists.')
+    process.exit(1)
+  }
+  fs.readFile(contentFile, 'utf8', (error, data) => {
+    const line = data.split(/\n/)
+    for (let i = 0; i < line.length; i += 1) {
+      result[i] = `${i + 1}: ${line[i]}`
     }
+    fs.writeFileSync(outputFile, result.join('\n'), {
+      encoding: 'utf8',
+      flag: 'w',
+    })
+    console.log('Overwriting of file is done.')
+  })
+} else {
+  fs.readFile(contentFile, 'utf8', (error, data) => {
+    const line = data.split(/\n/)
+    for (let i = 0; i < line.length; i += 1) {
+      result[i] = `${i + 1}: ${line[i]}`
+    }
+    fs.writeFileSync(outputFile, result.join('\n'), {
+      encoding: 'utf8',
+      flag: 'w',
+    })
+    console.log('Created ouput file')
   })
 }
