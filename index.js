e58db700e05ada82f2e62b58d8973e9de1f46578
Enrico Sebastian S. Digman <62365927+enricodigman@users.noreply.github.com>
2022-10-25 14:53:54 +0800

    Fs branch - part 2 activity (#1)

---
+++ index.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+
+const [,, contentFile, outputFile, option] = process.argv
+
+const result = []
+if (contentFile === undefined || outputFile === undefined) {
+  console.log('Missing arguments')
+  process.exit(1)
+}
+if (fs.existsSync(outputFile)) {
+  if (option !== '-y' && option !== '-n' && option !== undefined) {
+    console.log('Third argument is only limited to -y and -n.')
+    process.exit(1)
+  }
+  if (option === undefined || option === '-n') {
+    console.log('The file already exists.')
+    process.exit(1)
+  }
+  fs.readFile(contentFile, 'utf8', (error, data) => {
+    const line = data.split(/\n/)
+    for (let i = 0; i < line.length; i += 1) {
+      result[i] = `${i + 1}: ${line[i]}`
+    }
+    fs.writeFileSync(outputFile, result.join('\n'), {
+      encoding: 'utf8',
+      flag: 'w',
+    })
+    console.log('Overwriting of file is done.')
+  })
+} else {
+  fs.readFile(contentFile, 'utf8', (error, data) => {
+    const line = data.split(/\n/)
+    for (let i = 0; i < line.length; i += 1) {
+      result[i] = `${i + 1}: ${line[i]}`
+    }
+    fs.writeFileSync(outputFile, result.join('\n'), {
+      encoding: 'utf8',
+      flag: 'w',
+    })
+    console.log('Created ouput file')
+  })
+}
